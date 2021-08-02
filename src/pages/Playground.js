96e6154d0bd03e6fda09cebb40ebc9d7a5971bc2
frimpongopoku <pongofrimi@gmail.com>
2021-08-02 13:54:39 +0400

    Setup boiler plate, css, folders, router, fonts, icons. etc :ok_hand:

---
+++ src/pages/Playground.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Footer from "../shared/components/Footer";
+
+export default class Playground extends Component {
+  render() {
+    return (
+      <div>
+        <center>This is the end, hold your breathe and count to 10</center>
+        <Footer />
+      </div>
+    );
+  }
+}
