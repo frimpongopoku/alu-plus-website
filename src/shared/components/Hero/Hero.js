657384552bcf82c36d6a4b13e8ff1ec366dcd0be
frimpongopoku <pongofrimi@gmail.com>
2021-08-09 11:10:51 +0400

    Hero officially done and will change with props

--- src/shared/components/Hero/Hero.js
+++ src/shared/components/Hero/Hero.js
@@ -5,27 +5,69 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 export default class Hero extends Component {
   render() {
+    const { theme, skewed, title, subtitle, link, linkText, Jsx } = this.props;
     return (
       <div style={{ position: "relative" }}>
-        <div className="hero-background">
-          <img src={backImage} className="hero-back-image" />
+        <div className="hero-sizing hero-background">
+          <img src={backImage} className="hero-sizing hero-back-image" />
         </div>
         <div
-          className="hero-trans-layer"
-          style={{ "--backgroundColor": "maroon" }}
+          className="hero-sizing hero-trans-layer"
+          style={{
+            "--backgroundColor": theme?.layerBackgroundColor || "maroon",
+          }}
         ></div>
-        <div className="skewed-box">
 
-        </div>
-        {/* <div className="hero-content">
-          <h2>AFRICAN LEADERSHIP UNIVERSITY</h2>
-          <p>Get the education that is built around the impact you want</p>
+        {skewed ? (
+          <>
+            <div
+              className="skewed-box"
+              style={{
+                "--skewed-background": theme?.skewedBackgroundColor || "maroon",
+              }}
+            ></div>
 
-          <button className="">
-            OUR PROGRAMS <FontAwesomeIcon icon={faLongArrowAltRight} />
-          </button>
-        </div> */}
+            <div className="hero-sizing content-box-2">
+              {!Jsx ? (
+                <>
+                  <h2 className="hero-title">{title}</h2>
+                  <p className="hero-subtitle">{subtitle}</p>
+                </>
+              ) : (
+                <Jsx />
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="hero-sizing hero-content">
+              {!Jsx ? (
+                <>
+                  <h2 className="hero-title">{title}</h2>
+                  <p className="hero-subtitle">{subtitle}</p>
+
+                  {linkText && link && (
+                    <a href={link} className="">
+                      {linkText} <FontAwesomeIcon icon={faLongArrowAltRight} />
+                    </a>
+                  )}
+                </>
+              ) : (
+                <Jsx />
+              )}
+            </div>
+          </>
+        )}
       </div>
     );
   }
 }
+
+Hero.defaultProps = {
+  skewed: true,
+  title: "AFRICAN LEADERSHIP UNIVERSITY",
+  subtitle: "Get the education that is built around the impact you want",
+  link: "#",
+  linkText: "OUR PROGRAMS",
+  Jsx: null,
+};
